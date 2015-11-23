c6e15f3f92ff430d20faae5aec73e13001ec7faf
Ruwan <ruwan.lokusooriya@finance.gov.au>
2015-11-23 13:46:48 +1100

    FWBC-4

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <link rel="schema.dcterms" href="http://purl.org/dc/terms/">
+  <link rel="schema.dcterms" href="http://purl.org/dc/terms/" />
     <?php print $head; ?>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<?php
