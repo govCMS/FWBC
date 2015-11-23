b53680c83e5d8ab0d9b16662a4dac2142f8be266
Ruwan <ruwan.lokusooriya@finance.gov.au>
2015-11-23 11:50:30 +1100

    fixing addthis share button

--- templates/block-breadcrumb.tpl.php
+++ templates/block-breadcrumb.tpl.php
@@ -19,7 +19,7 @@
 		<div id="pageTools">
 			<!-- AddThis Button BEGIN -->
 			<div class="addthis_toolbox addthis_default_style"><a class="addthis_button_compact"></a></div>
-			<script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=xa-50ac4079486b648f"></script>
+			<script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=xa-50ac4079486b648f"></script>
 			<!-- AddThis Button END --> 
 			<a id="printBtn" href="javascript:window.print();" title="Print"><img src="<?php theme_url(); ?>/images/printer.png" alt="Print" width="16" height="16" /><span class="hidden">Print</span></a>
 			<a id="smallTextIcon" class="selected" href="#" title="Text Normal"><img src="<?php theme_url(); ?>/images/text-normal.gif" alt="Text Normal" width="14" height="20" /><span class="hidden">Text Normal</span></a> 
