71fb2d9efdb5f96cc6c13a6bf941480d8baf4815
Joseph Zhao <joseph.zhao@xing.net.au>
2015-11-26 11:36:10 +1100

    HTML5 clean up

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,19 +1,12 @@
 <!DOCTYPE html>
-<!--[if IEMobile 7]>
-<html class="iem7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if lte IE 6]>
-<html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (IE 7)&(!IEMobile)]>
-<html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
-<!--[if IE 8]>
-<html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)]><!-->
-<html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
 
 <head>
-  <link rel="schema.dcterms" href="http://purl.org/dc/terms/"/>
   <?php print $head; ?>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <?php
   $parent_title = "";
   if (arg(0) == 'node' && is_numeric(arg(1))) {
@@ -31,25 +24,27 @@
       echo $parent_title . " | ";
     }
     echo $head_title; ?></title>
-  <meta name="viewport" content="width=device-width"/>
-  <link rel="shortcut icon" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon.ico"/>
-  <link rel="apple-touch-icon" sizes="57x57" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-57x57.png"/>
-  <link rel="apple-touch-icon" sizes="60x60" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-60x60.png"/>
-  <link rel="apple-touch-icon" sizes="72x72" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-72x72.png"/>
-  <link rel="apple-touch-icon" sizes="76x76" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-76x76.png"/>
-  <link rel="apple-touch-icon" sizes="114x114" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-114x114.png"/>
-  <link rel="apple-touch-icon" sizes="120x120" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-120x120.png"/>
-  <link rel="apple-touch-icon" sizes="144x144" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-144x144.png"/>
-  <link rel="apple-touch-icon" sizes="152x152" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-152x152.png"/>
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-180x180.png"/>
-  <link rel="icon" type="image/png" sizes="192x192" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/android-icon-192x192.png"/>
-  <link rel="icon" type="image/png" sizes="32x32" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon-32x32.png"/>
-  <link rel="icon" type="image/png" sizes="96x96" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon-96x96.png"/>
-  <link rel="icon" type="image/png" sizes="16x16" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon-16x16.png"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon.ico">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/favicon-16x16.png">
   <link rel="manifest" href="<?php print '/' . $base_path . path_to_theme(); ?>/icons/manifest.json"/>
-  <meta name="msapplication-TileColor" content="#ffffff"/>
-  <meta name="msapplication-TileImage" content="<?php print '/' . $base_path . path_to_theme(); ?>/icons/ms-icon-144x144.png"/>
-  <meta name="theme-color" content="#ffffff"/>
+  <link rel="schema.dcterms" href="http://purl.org/dc/terms/">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?php print '/' . $base_path . path_to_theme(); ?>/icons/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
   <?php echo $styles; ?>
   <?php echo $scripts; ?>
 </head>
