60ebe3bca72e3a373fa8fb69e6127bbd065e98fd
Karl Hepworth <karl.hepworth@gmail.com>
2017-01-19 10:01:07 +1100

    changes to industry update header/page
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- templates/page--single_newsletter_section.tpl.php
+++ templates/page--single_newsletter_section.tpl.php
@@ -11,8 +11,8 @@ $node_data = $variables['node'];
 		<div id="newsletterContent">
 			<div class="newsletterHeadingBox">
 				<div class="social"><a href="http://www.facebook.com/share.php?u=<?php echo fwbc_page_url('node/'.$node_data->nid); ?>&t=<?php echo urlencode($node_data->title); ?>" target="_blank"><img src="<?php fwbc_theme_url(); ?>/images/facebook.png" width="16" height="16" alt="facebook button" /></a> <a href="http://twitter.com/share?url=<?php echo fwbc_page_url('node/'.$node_data->nid); ?>&title=<?php echo urlencode($node_data->title); ?>" target="_blank"><img src="<?php fwbc_theme_url(); ?>/images/twitter.png" width="16" height="16" alt="twitter button" /></a> <a href="http://www.addthis.com/bookmark.php" class="addthis_button_email"><img src="<?php fwbc_theme_url(); ?>/images/email.png" width="16" height="16" alt="Email Button"></a></div>
-				<img src="<?php fwbc_theme_url(); ?>/images/industry_update.jpg" alt="Industry Update" width="685" height="125" />
-				<h1>Industry <br />Update</h1>
+				<img src="<?php fwbc_theme_url(); ?>/images/assets/banner-industry-update.jpg" alt="Industry Update" width="685" height="125" />
+				<h1 class="element-invisible">Industry <br />Update</h1>
 			</div>
 			<a name="top"></a>
 			<?php if ($tabs): ?>
